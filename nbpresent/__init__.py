25da1062c7df859ffe2d4ae2f081e3b72f4570bd
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-11-09 17:25:14 -0500

    might be ready to download a zipped, standalone show

--- nbpresent/__init__.py
+++ nbpresent/__init__.py
@@ -1,2 +1,8 @@
 # flake8: noqa
+from nbconvert.exporters.export import exporter_map
+
 from ._version import __version__, __version_info__
+from .exporter import PresentExporter
+
+def load_jupyter_server_extension(nbapp):
+    exporter_map["nbpresent"] = PresentExporter
