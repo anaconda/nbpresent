2d1697751fbbac3a974e1eac66699fd69c697d0a
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-11-12 15:43:00 -0500

    fixes found in screencast

--- bin/screecast.sh
+++ bin/screecast.sh
@@ -1,1 +1,7 @@
-byzanz-record -w 1200 -h 768 -x 5 -y 105 nbpresent/static/nbpresent.gif -d 30
+byzanz-record \
+  -w 1195 \
+  -h 768 \
+  -x 8 \
+  -y 105 \
+  -d 100 \
+  nbpresent/static/nbpresent.gif
