4c3de4d388cbe681d54d8c5c7e2a2c36062e343b
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-12-13 00:16:03 -0500

    adding more utils

--- nbpresent/tests/js/_utils.js
+++ nbpresent/tests/js/_utils.js
@@ -1,11 +1,21 @@
 ;(function(){
   "use strict";
-  var root = this.nbpresent ? this.nbpresent : this.nbpresent = {};
+  var root = this.nbpresent_utils ? this.nbpresent_utils :
+    this.nbpresent_utils = {};
 
   var img = 0;
 
   root.screenshot = function(){
     this.captureSelector("screenshots/capture_" + (img++) + ".png", "body");
+  }
+
+  root.canSeeAndClick = function(message, visible, click){
+    return this
+      .waitUntilVisible(visible)
+      .then(function(){
+        t.assertExists(click || visible, "I can see and click " + message);
+        this.click(click || visible);
+      });
   }
 
 }).call(this);
--- nbpresent/tests/js/test_create.js
+++ nbpresent/tests/js/test_create.js
@@ -3,47 +3,23 @@ var t = casper.test;
 function create_test(){
   var img = 0;
 
-  var screenshot = nbpresent.screenshot.bind(this);
-  
+  var screenshot = nbpresent_utils.screenshot.bind(this);
+
+  this.canSeeAndClick = nbpresent_utils.canSeeAndClick.bind(this);
+
   // the actual test
   this
-    .waitUntilVisible("#nbpresent_sorter_btn")
-    .then(function(){
-      t.assertExists("#nbpresent_sorter_btn", "the sorter button");
-      this.click("#nbpresent_sorter_btn");
-    })
-    .waitUntilVisible(".tour-nbpresent")
-    .then(function(){
-      t.assertExists(".tour-nbpresent", "the tour");
-      screenshot();
-      this.click(".tour-nbpresent .popover-navigation > .btn");
-    })
+    .canSeeAndClick("the sorter button", "#nbpresent_sorter_btn")
+    .canSeeAndClick("the tour",
+      ".tour-nbpresent", ".tour-nbpresent .popover-navigation > .btn")
     .waitWhileVisible(".tour-nbpresent")
-    .waitUntilVisible(".nbpresent_sorter")
-    .then(function(){
-      t.assertExists(".deck_toolbar .fa-plus-square-o", "the add slide button");
-      screenshot();
-      this.click(".deck_toolbar .fa-plus-square-o");
-    })
-    .waitUntilVisible(".nbpresent_template_library")
-    .then(function(){
-      t.assertExists(".nbpresent_template_library .slide", "the library");
-      screenshot();
-      this.click(".nbpresent_template_library .slide");
-    })
+    .canSeeAndClick("the add slide button", ".deck_toolbar .fa-plus-square-o")
+    .canSeeAndClick("a slide in the library",
+      ".nbpresent_template_library .slide")
     .waitWhileVisible(".nbpresent_template_library")
-    .waitUntilVisible(".slides_wrap .slide")
-    .then(function(){
-      t.assertExists(".deck_toolbar .fa-edit", "the edit button");
-      screenshot();
-      this.click(".deck_toolbar .fa-edit");
-    })
-    .waitUntilVisible(".nbpresent_regiontree")
-    .then(function(){
-      t.assertExists(".nbpresent_regiontree", "the region tree");
-      screenshot();
-      this.click(".nbpresent_regiontree .region");
-    });
+    .canSeeAndClick("the edit button", ".deck_toolbar .fa-edit")
+    .canSeeAndClick("a region in the region tree",
+      ".nbpresent_regiontree .region")
 }
 
 
