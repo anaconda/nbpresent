c7231e076ed29b935f04fe2b300a9e767c60f54f
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-12-15 09:52:39 -0500

    fixing up conda build

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -3,5 +3,6 @@ cd $SRC_DIR
 npm install .
 npm run lint
 npm run dist
+python setup.py develop
 npm run docs
 python setup.py install
--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
                  "License :: OSI Approved :: BSD License"],
     packages=["nbpresent"],
     include_package_data=True,
-    install_requires=["notebook"],
-    tests_require=["flake8", "nose"],
     entry_points={
         'console_scripts': ['nbpresent=nbpresent.export:main'],
     }
