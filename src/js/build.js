e42b5cab204693d40a9cbb4337e1d569dd62e31a
Nicholas Bollweg <nbollweg@continuum.io>
2016-01-27 17:55:56 -0500

    keeping up with the testing out in the future

--- src/js/build.js
+++ src/js/build.js
@@ -3,7 +3,8 @@
     paths: {
       "nbpresent-deps": "../../nbpresent/static/nbpresent/nbpresent.deps.min",
       "nbpresent-standalone": "../../nbpresent/static/nbpresent/nbpresent.standalone.min",
-      "jquery" : "../../node_modules/jquery/dist/jquery"
+      "jquery" : "../../node_modules/jquery/dist/jquery",
+      "underscore" : "../../node_modules/underscore/underscore"
     },
     name: "main",
     out: "../../nbpresent/static/nbpresent/nbpresent.static.min.js",
