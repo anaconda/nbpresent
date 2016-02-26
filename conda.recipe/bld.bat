882c96a69ead1d8c5879c8f1341ce0122c3bc6c1
Nicholas Bollweg <nbollweg@continuum.io>
2016-02-26 14:44:40 -0500

    actually do need to clean node_modules

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,2 +1,2 @@
-"%PREFIX%\Scripts\npm.cmd" install -g npm@latest && "%PREFIX%\Scripts\npm.cmd" install . && "%PREFIX%\Scripts\npm.cmd" run build:release && "%PYTHON%" setup.py install
+"%PREFIX%\Scripts\npm.cmd" install -g npm@latest && "%PREFIX%\Scripts\npm.cmd" install . && "%PREFIX%\Scripts\npm.cmd" run build:release && "%PYTHON%" setup.py install && rmdir "%PREFIX%\node_modules" /s /q
 if errorlevel 1 exit 1
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,4 +1,5 @@
 npm install -g npm@latest
 npm install .
 npm run build:release
+rm -rf "${PREFIX}/node_modules"
 "${PYTHON}" setup.py install
