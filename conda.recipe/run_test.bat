e42d472e5b3469edd743e61d20021279b962e5d0
Nicholas Bollweg <nbollweg@continuum.io>
2016-02-29 08:32:16 -0500

    line endings for windows

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,2 +1,4 @@
-"%PREFIX%\Scripts\npm.cmd" install -g npm@latest && "%PREFIX%\Scripts\npm.cmd" install . && "%PREFIX%\Scripts\npm.cmd" run test
+"%PREFIX%\Scripts\npm.cmd" install -g npm@latest
+"%PREFIX%\Scripts\npm.cmd" install .
+"%PREFIX%\Scripts\npm.cmd" run test
 if errorlevel 1 exit 1
