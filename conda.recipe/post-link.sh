6582be1359ed8391429c0c3c5f0c0fe51143afa1
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-07 23:43:50 -0500

    oh, pip

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,4 +1,4 @@
-"${PREFIX}/bin/pip" install -e git@github.com:ellisonbg/notebook.git@nbext-pain#egg=NotebookFortyTwo
+"${PREFIX}/bin/pip" install -e git+git@github.com:ellisonbg/notebook.git@nbext-pain#egg=NotebookFortyTwo
 
 "${PREFIX}/bin/jupyter" nbextension install --py="${PKG_NAME}" --overwrite --sys-prefix
 "${PREFIX}/bin/jupyter" nbextension enable --py="${PKG_NAME}" --sys-prefix
--- conda.recipe/run_test.sh
+++ conda.recipe/run_test.sh
@@ -1,4 +1,4 @@
-"${PREFIX}/bin/pip" install -e git@github.com:ellisonbg/notebook.git@nbext-pain#egg=NotebookFortyTwo
+"${PREFIX}/bin/pip" install -e git+git@github.com:ellisonbg/notebook.git@nbext-pain#egg=NotebookFortyTwo
 "${PREFIX}/bin/npm" install -g npm@latest --no-progress --no-spin
 "${PREFIX}/bin/npm" install . --no-progress --no-spin
 "${PREFIX}/bin/npm" run test --no-progress --no-spin
