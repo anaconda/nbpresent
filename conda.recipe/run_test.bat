90a0cc3c062d32bc532adcf8eb3c8ffd6efbdcf4
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-08 01:04:54 -0500

    more fiddling

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,4 +1,4 @@
-"%PREFIX%\Scripts\pip" install https://github.com/ellisonbg/notebook/archive/nbext-pain.zip
+"%PREFIX%\Scripts\pip" install -e git+https://github.com/ellisonbg/notebook.git#egg=notebook
 "%PREFIX%\Scripts\npm.cmd" install -g npm@latest --no-spin
 "%PREFIX%\Scripts\npm.cmd" install . --no-spin --no-progress
 "%PREFIX%\Scripts\npm.cmd" run test --no-progress --no-spin
--- conda.recipe/run_test.sh
+++ conda.recipe/run_test.sh
@@ -1,4 +1,4 @@
-"${PREFIX}/bin/pip" install  https://github.com/ellisonbg/notebook/archive/nbext-pain.zip
+"${PREFIX}/bin/pip" install -e git+https://github.com/ellisonbg/notebook.git#egg=notebook
 "${PREFIX}/bin/npm" install -g npm@latest --no-progress --no-spin
 "${PREFIX}/bin/npm" install . --no-progress --no-spin
 "${PREFIX}/bin/npm" run test --no-progress --no-spin
