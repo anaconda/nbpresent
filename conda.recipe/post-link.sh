1507acd201727e44b50d70ce92a8091d0a01fcab
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-08 01:27:57 -0500

    try locally-built notebook

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,7 +1,3 @@
-"${PREFIX}/bin/pip" install -e git+https://github.com/ellisonbg/notebook.git#egg=notebook
-
 "${PREFIX}/bin/jupyter" nbextension install --py="${PKG_NAME}" --overwrite --sys-prefix
 "${PREFIX}/bin/jupyter" nbextension enable --py="${PKG_NAME}" --sys-prefix
 "${PREFIX}/bin/jupyter" serverextension enable --py="${PKG_NAME}" --sys-prefix
-
-"${PREFIX}/bin/pip" uninstall notebook -y
--- conda.recipe/run_test.sh
+++ conda.recipe/run_test.sh
@@ -1,4 +1,3 @@
-"${PREFIX}/bin/pip" install -e git+https://github.com/ellisonbg/notebook.git#egg=notebook
 "${PREFIX}/bin/npm" install -g npm@latest --no-progress --no-spin
 "${PREFIX}/bin/npm" install . --no-progress --no-spin
 "${PREFIX}/bin/npm" run test --no-progress --no-spin
