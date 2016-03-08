343b7c7db25e6eb3e5edc21cf2695f6c15798dc9
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-07 23:39:01 -0500

    trying pip install/uninstall. ugh.

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,3 +1,7 @@
+"${PREFIX}/bin/pip" install -e git@github.com:ellisonbg/notebook.git@nbext-pain#egg=NotebookFortyTwo
+
 "${PREFIX}/bin/jupyter" nbextension install --py="${PKG_NAME}" --overwrite --sys-prefix
 "${PREFIX}/bin/jupyter" nbextension enable --py="${PKG_NAME}" --sys-prefix
 "${PREFIX}/bin/jupyter" serverextension enable --py="${PKG_NAME}" --sys-prefix
+
+"${PREFIX}/bin/pip" uninstall notebook
