040c1df273da82d694539118deb367bf7bc66d3f
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-13 08:33:40 -0400

    actually provide entry points

--- nbpresent/__init__.py
+++ nbpresent/__init__.py
@@ -1,13 +1,12 @@
 # coding: utf-8
 # flake8: noqa
-from ._version import __version__, __version_info__
-
 from nbconvert.exporters.export import exporter_map
 
 from .exporters.html import PresentExporter
+from ._version import __version__, __version_info__
+
 
 PDFPresentExporter = None
-
 pdf_import_error = None
 
 try:
@@ -15,6 +14,21 @@ try:
 except Exception as err:
     pdf_import_error = err
 
+
+# Jupyter Extension points
+def _jupyter_nbextension_paths():
+    return [dict(
+        section="notebook",
+        src=os.path.join("static", "nbpresent"),
+        dest="nbpresent",
+        require="nbpresent/js/nbpresent.min")]
+
+
+def _jupyter_server_extension_paths():
+    return [dict(module="nbpresent")]
+
+
+# serverextension
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("✓ nbpresent HTML export ENABLED")
     exporter_map.update(
