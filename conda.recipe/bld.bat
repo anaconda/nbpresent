3220c6a9d76d04212c4e78fc14b9cccb9a606f68
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-09 11:18:08 -0400

    eaten by a grue

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,17 +1,16 @@
+pushd .
+cd /D %PREFIX%\..\pkgs
+rmdir /s /q "\\?\%cd%\.trash" || echo "no trash to delete"
+popd
+
 CALL npm install --parseable || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 CALL npm run build:release  || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-rmdir "node_modules" /s /q
+rmdir /s /q "\\?\%cd%\node_modules"
 "%PYTHON%" setup.py install
 
 CALL "%PREFIX%\Scripts\jupyter-nbextension" install nbpresent --py --sys-prefix --overwrite || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-REM force clean of pkg trash
-pushd
-cd %PREFIX%\..\pkgs
-rmdir /s /q "\\?\%cd%\.trash"
-popd
--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -5,7 +5,4 @@ CALL npm run test || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 REM force clean of pkg trash
-pushd
-cd %PREFIX%\..\pkgs
-rmdir /s /q "\\?\%cd%\.trash"
-popd
+rmdir /s /q "\\?\%cd%\node_modules"
