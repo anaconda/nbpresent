547edc9075ef0ffc0242ca1084e2430db6e06bc0
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-19 17:04:00 -0400

    setup.py args

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,5 +1,5 @@
 npm install
 npm run build:release
 rm -rf node_modules
-"${PYTHON}" setup.py install
+"${PYTHON}" setup.py install --single-version-externally-managed --record=record.txt
 "${PREFIX}/bin/jupyter-nbextension" install nbpresent --py --sys-prefix --overwrite
